eedbeee671b00ccc1cfae2ae214dc0b7b7fa3c28
unknown <guyuanhao2016@gmail.com>
2017-08-14 15:00:26 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import {HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule,
+                  FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+                ],
+  declarations: [ AppComponent,
+                  HeroesComponent,
+                  HeroDetailComponent
+                ],
+  bootstrap:    [ AppComponent ],
+  providers : [ HeroService ]
 })
 export class AppModule { }
